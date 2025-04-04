a341c04558955b12cf8836dd33644cf267d0235c
JohnRendell <catuiza.johnrendell@gmail.com>
2025-04-04 10:06:15 +0800

    Set up react-native expo

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, Platform } from "react-native";
+import Helmet from "react-helmet";
+
+export default function index(){
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        {Platform.OS === "web" && (
+          <Helmet>
+            <title>SnapGrid</title>
+          </Helmet>
+        )}
+      <Text>Hello world, this is a front page</Text>
+    </SafeAreaView>
+  )
+}
+
