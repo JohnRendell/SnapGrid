1e074caefecda30c79ad73b1ffc312fb96bf6465
JohnRendell <catuiza.johnrendell@gmail.com>
2025-04-09 19:29:36 +0800

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -35,8 +35,6 @@ export default function index() {
         </Helmet>
       )}
 
-            <Login_Page />
-
       {/* Logo Image */}
       <Image
         source={require("./Images/Logo.png")}
